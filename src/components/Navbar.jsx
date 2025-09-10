cd20cf0b6530c6ac9d01ca8bb7fa95b25a6c287d
edermelo-git <eder.worksites@gmail.com>
2025-09-10 19:41:31 -0300

    Header content alignment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,12 @@ const Navbar = () => {
             <a href="/" className="logo"> <img src={logo} alt="Logo do site" /> </a>
 
             <nav className="navbar">
-                <a href="/">Inicio</a>
+                <a href="/">Início</a>
                 <a href="/">Modelos</a>
+                <a href="/">IPads</a>
+                <a href="/">MacBook</a>
                 <a href="/">Sobre</a>
-                <a href="/">Entre em contato</a>
+                <a href="/">contato</a>
             </nav>
 
 
