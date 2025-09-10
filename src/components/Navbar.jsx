0327b8884bca823be1a7abc23c8a5b445762b1fc
edermelo-git <eder.worksites@gmail.com>
2025-09-10 04:34:47 -0300

    First steps taken

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./Navbar.css";
+import logo from "../assets/logosite.png";
+
+
+const Navbar = () => {
+    return (
+
+        <header className="header">
+            <a href="/" className="logo"> <img src={logo} alt="Logo do site" /> </a>
+
+            <nav className="navbar">
+                <a href="/">Inicio</a>
+                <a href="/">Produtos</a>
+                <a href="/">Sobre</a>
+                <a href="/">Entre em contato</a>
+            </nav>
+
+
+        </header>
+
+    )
+}
+
+export default Navbar;
